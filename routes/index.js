c8bbce15c9d8423e97398f7f5435babb2245cb45
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-06-17 12:59:55 +1000

    end of class

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const ProductController = require('./../controllers/product_controller');
+const { celebrate, Joi } = require("celebrate");
 
-router.get("/", (req, res) => res.send("Welcome"));
+router.get("/", (req, res, next) => {
+    res.send("Welcome")
+});
 
 router.get("/products", ProductController.index);
 
-router.post("/products", ProductController.create);
+router.post("/products", celebrate({
+    body: {
+        name: Joi.string().required(),
+        price: Joi.number().greater(0)
+    },
+    query: {
+        test: Joi.string().valid("garret").required()
+    }
+}), ProductController.create);
 
 router.get("/products/:id", ProductController.show);
 
